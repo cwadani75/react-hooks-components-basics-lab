1acbc74a00ee346a943da4aa18bae4f4d1deafe2
cwadani75 <cwadani75@gmail.com>
2025-04-15 14:40:32 +0300

    hooks-components

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+      <p>This is a simple portfolio app built with React components. You can learn more about me and my work here.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div id="home">
+      <h2>Welcome to My Portfolio</h2>
+      <p>This is the home section where you can find an introduction.</p>
+    </div>
+  );
+}
+
+export default Home;
